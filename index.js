e8303ead453d60957e28a0f84af879f42e930c07
LoboLuciano <lucholobo2016@gmail.com>
2020-06-23 07:32:01 -0300

    agrega index.html

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express();
 // Port
 const port = process.env.PORT || 3000;
 
+app.use(express.static('public'));
+
 // Mensaje
 app.get('/', (req, res) => {
     res.send('Hello world'); 
